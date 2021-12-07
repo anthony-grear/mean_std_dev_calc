f7a0e2ff685422ed7a0db912f982b121d3f2d14b
Anthony Grear <tgrear2008@gmail.com>
2021-12-07 15:12:54 +0700

    Change 2nd JFrame to JDialog for result output

--- mean_std_dev/src/mean_std_dev/MainFrame.java
+++ mean_std_dev/src/mean_std_dev/MainFrame.java
@@ -1,34 +1,45 @@
 package mean_std_dev;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainFrame {
-	private JFrame frame1, frame2; 
+	private JFrame frame1; 
 	private JPanel panel1, panel2;
+	private JDialog dialog;
 	
 	public MainFrame() {
 		initialize();		
 	}
 
 	private void initialize() {
+		
+		//frame to enter values
 		frame1 =  new JFrame();
 		frame1.setTitle("Mean and Std Deviation Calculator");
 		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame1.setSize(400,185);
+		frame1.setSize(400,185);
 		frame1.setLocation(450,100);
 		frame1.setLayout(new BorderLayout());
 		
-		frame2 = new JFrame();
-		frame2.setTitle("Output Window");
-		frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//		frame2.setSize(300,185);
-		frame2.setLocation(850,100);
-		frame2.setLayout(new BorderLayout());
+		//container for frame1
+		panel1 = new JPanel();
+		panel1.setBackground(new Color(31, 104, 150));
 		
-		frame1.pack();
+		//frame to display window
+		dialog = new JDialog(frame1, "Mean and Std Dev");
+//		dialog.setTitle("Output Window");
+		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		dialog.setSize(300,185);
+		dialog.setLocation(850,100);
+		dialog.setLayout(new BorderLayout());
+		dialog.setVisible(true);
+		
+//		frame1.pack();
 		frame1.setVisible(true);
 		
 	}
