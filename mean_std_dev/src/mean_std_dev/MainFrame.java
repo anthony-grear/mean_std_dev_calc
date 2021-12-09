edd4ce267401a211a6e78cc163d9538056c97304
Anthony Grear <tgrear2008@gmail.com>
2021-12-09 15:47:45 +0700

    Add try catch block to validate user input.

--- mean_std_dev/src/mean_std_dev/MainFrame.java
+++ mean_std_dev/src/mean_std_dev/MainFrame.java
@@ -17,16 +17,14 @@ import java.awt.event.ActionEvent;
 
 public class MainFrame {
 	private JFrame frame1; 
-	private JPanel panel1, panel2;
-	private JDialog dialog;
+	private JPanel panel1, panel2, panel3;
+	private JDialog dialog, dialog2;
 	private JTextField tf;
 	private JButton button;
 	private JLabel label;
-	double total=0;
-	double total2=0;
-	double tempDouble;
-	double avg = 0;
-	String temp = new String();
+	private JTextArea textArea = new JTextArea();
+	
+	
 	
 	public MainFrame() {
 		initialize();		
@@ -52,8 +50,16 @@ public class MainFrame {
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				try {
+				double total=0;
+				double total2=0;
+				double tempDouble;
+				double avg = 0;
+				String temp = new String();				
 				String values = tf.getText();
-				String[] arr = values.split(",");				
+				String[] arr = values.split(",");
+				
+				textArea.setText("");
 				
 				for (int i=0;i<arr.length; i++) {
 					temp = arr[i].trim();
@@ -66,42 +72,50 @@ public class MainFrame {
 					tempDouble = Double.parseDouble(temp);
 					total2 = total2 + Math.pow((tempDouble-avg), 2);			
 				}
-				Double stdDev = Math.sqrt(total2/arr.length);
-				
-				
+				Double stdDev = Math.sqrt(total2/arr.length);				
 				tf.setText("");
 				String s=String.valueOf(avg);
-				String s2 = String.valueOf(stdDev);
-				JTextArea textArea = new JTextArea ("Mean: "+s+"\nStdDev: "+s2);
+				String s2 = String.valueOf(stdDev);				
+				textArea.setText("Mean: "+s+"\nStdDev: "+s2);
 				panel2.add(textArea);
 				dialog.setVisible(true);
+				} catch (Exception e3) {
+					dialog2.setVisible(true);
+					tf.setText("");
+					
+				}
 			}
 		});		
 		
-		
+		label = new JLabel("<html>Incorrect Input. <br>Enter the list of numbers, separated by commas.<html>");
 		//container for frame1
 		panel1 = new JPanel();
-//		panel1.setBackground(new Color(31, 104, 150));
 		panel1.add(tf);
 		panel1.add(button);
 		JTextArea textArea2 = new JTextArea("Enter a list of numbers separated by commas.");
 		panel1.add(textArea2);
-		
 		panel2 = new JPanel();
+		panel3 = new JPanel();
+		panel3.add(label);
 		
+		
+		dialog2 = new JDialog(frame1, "Input Error");
+		dialog2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		dialog2.setLocation(250,100);
+		dialog2.setLayout(new BorderLayout());
+		dialog2.add(panel3);
+		dialog2.pack();
 		
 		//dialog to display output
 		dialog = new JDialog(frame1, "Mean and Std Dev");
-//		dialog.setTitle("Output Window");
 		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		dialog.setSize(300,185);
 		dialog.setLocation(850,100);
 		dialog.setLayout(new BorderLayout());
-		dialog.add(panel2);
-		
+		dialog.add(panel2);		
 		
 		frame1.add(panel1);
-//		frame1.pack();
 		frame1.setVisible(true);		
 	}
 	
